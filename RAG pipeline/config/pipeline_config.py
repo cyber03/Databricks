e0c56a8f169a7fcb4befdada879f3bcdb537cd79
cyber03 <harishsomalinga@gmail.com>
2026-06-08 18:07:44 +0000

    RAG - Bronze_layer

---
+++ RAG pipeline/config/pipeline_config.py
@@ -0,0 +1,9 @@
+# config/pipeline_config.py
+
+CATALOG           = "rag"
+SCHEMA            = "bronze"
+
+RAW_DATA_PATH     = "/Volumes/rag/bronze/raw_pdf/"
+BRONZE_TABLE      = "rag.bronze.docs"
+
+CHECKPOINT_PATH   = "/Volumes/rag/bronze/_checkpoints/"
